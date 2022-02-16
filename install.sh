188984c6e322e6046c466e2ee4435a575dd30662
Shinichi Hatano <shinichi.hatano@ctc-g.co.jp>
2022-02-16 13:58:51 +0900

    add cloning process to install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,36 @@
 #!/usr/bin/env bash
+#===================================================================================
+#
+# FILE: install.sh
+#
+# USAGE: install.sh
+#
+# DESCRIPTION: 
+#===================================================================================
 
 set -e
 
-printf -- "first install script\n" 
+
+#----------------------------------------------------------------------
+# Constants
+#----------------------------------------------------------------------
+DOT_WORK_DIR="$(cd "$(dirname "$0")"; pwd)"
+DOT_DIR="~/.dotfiles"
+
+
+#----------------------------------------------------------------------
+# Main
+#----------------------------------------------------------------------
+
+if [ ! -d ${DOT_DIR} ]; then
+  if [ ! -z `type -p git` ]; then
+    git clone https://github.com/rising1008/dotfiles.git ${DOT_DIR}
+  else
+    printf -- 'You don\'t seem to have git installed.\n;
+    printf -- 'Install it: xcode-select --install\n'
+    exit 127
+  fi
+else
+  echo "dotfiles already exists"
+  exit 1
+fi
