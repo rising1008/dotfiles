dc59eb931cf43528afe7ed2dcfcca766a65cc997
Shinichi Hatano <shinichi.hatano@ctc-g.co.jp>
2022-07-23 16:25:08 +0900

    modified

--- install.sh
+++ install.sh
@@ -28,16 +28,16 @@ banner() {
   printf -- "\n"
   printf -- "_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/\n\n"
   printf -- "  DOTFILES\n"
-  printf -- "  ${VERSION}\n\n"
+  printf -- "  %s\n\n" "${VERSION}"
   printf -- "_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/\n\n"
 }
 
 clone() {
-  local readonly TARGET_DIR=$1
+  local TARGET_DIR=$1
 
   printf -- '  Clonning the dotfiles repository... \n\n';
-  if [ ! -d ${TARGET_DIR} ]; then
-    git clone https://github.com/rising1008/dotfiles.git ${TARGET_DIR}
+  if [ ! -d "${TARGET_DIR}" ]; then
+    git clone https://github.com/rising1008/dotfiles.git "${TARGET_DIR}"
   else
     echo "    ${ESC}[33mdotfiles already exists!${ESC}[m"
   fi
@@ -45,40 +45,39 @@ clone() {
 }
 
 macos() {
-  local readonly TARGET_DIR=$1
+  local TARGET_DIR=$1
 
   printf -- '  Setting the macos... \n\n';
-  bash ${TARGET_DIR}/settings/darwin/.macos
+  bash "${TARGET_DIR}/settings/darwin/.macos"
   printf -- '    done. \n\n';
 
 }
 
 application() {
-  local readonly TARGET_DIR=$1
+  local TARGET_DIR=$1
 
   printf -- '  Install homebrew... \n\n';
   [ ! -z `type -p brew` ] || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  brew bundle --file ${TARGET_DIR}/settings/darwin/Brewfile
+  brew bundle --file "${TARGET_DIR}/settings/darwin/Brewfile"
 
-  cp ${TARGET_DIR}/settings/darwin/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
-  ln -snf ${TARGET_DIR}/settings/darwin/config/nvim ${HOME}/.config/nvim
-  [ -d ${HOME}/.cache/dein/repos/github.com/Shougo/dein.vim ] || curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ${HOME}/installer.sh
-  [ -d ${HOME}/.cache/dein/repos/github.com/Shougo/dein.vim ] || sh ${HOME}/installer.sh ${HOME}/.cache/dein
-
+  cp "${TARGET_DIR}"/settings/darwin/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
+  ln -snf "${TARGET_DIR}"/settings/darwin/config/nvim "${HOME}"/.config/nvim
+  [ -d "${HOME}"/.cache/dein/repos/github.com/Shougo/dein.vim ] || curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > "${HOME}"/installer.sh
+  [ -d "${HOME}"/.cache/dein/repos/github.com/Shougo/dein.vim ] || sh "${HOME}"/installer.sh "${HOME}"/.cache/dein
   printf -- '    done. \n\n';
 }
 
 user() {
-  local readonly TARGET_DIR=$1
+  local TARGET_DIR=$1
 
-  for f in ${TARGET_DIR}/settings/darwin/*;
+  for f in "${TARGET_DIR}"/settings/darwin/*;
   do
       [[ "${f##*/}" == "Brewfile" ]] && continue
       [[ "${f##*/}" == ".Brewfile.lock.json" ]] && continue
       [[ "${f##*/}" == ".macos" ]] && continue
       [[ "${f##*/}" == ".config" ]] && continue
       [[ "${f##*/}" == "com.googlecode.iterm2.plist" ]] && continue
-  
+
       ln -snf "$f" "$HOME/.${f##*/}"
   done
 }
@@ -88,7 +87,7 @@ user() {
 : ----------------------------------------------------------------------
 banner
 
-clone ${DOT_DIR}
-macos ${DOT_DIR}
-application ${DOT_DIR}
-user ${DOT_DIR}
+clone "${DOT_DIR}"
+macos "${DOT_DIR}"
+application "${DOT_DIR}"
+user "${DOT_DIR}"
