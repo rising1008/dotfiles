8e804875baf66f44e9dd0ef90b49d6f9e2ba48ef
Rising <rising1008@gmail.com>
2023-10-14 18:07:07 +0900

    modified

--- install.sh
+++ install.sh
@@ -114,6 +114,10 @@ setup_macos () {
   ${SCRIPT_DIR}/bin/.macos
 }
 
+function setup_tmux() {
+  ln -snf "${SCRIPT_DIR}/settings/darwin/.tmux.conf" "${HOME}/.tmux.conf"
+}
+
 : --------------------------------------------------
 :  Main
 : --------------------------------------------------
@@ -125,18 +129,20 @@ p '  Install homebrew...\n' 'info';
 install_homebrew
 
 p '  Install packages...\n' 'info';
-install_packages
+# install_packages
 
 p '\n  Set up zsh...\n';
-setup_zsh
+# setup_zsh
 
 printf -- '  Set up neovim...\n';
-setup_neovim
+# setup_neovim
 
 printf -- '  Set up iTerms...\n';
 # 設定ファイルの場所を指定することで対応
 # setup_iterms "${SCRIPT_DIR}" "${BACKUP_DIR}"
 
+printf -- '  Set up tmux...\n';
+setup_tmux
 printf -- '  Set up git...\n';
 setup_git
 
