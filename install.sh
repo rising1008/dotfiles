7d9b4d772757f985a6d6c53739a2ef355f73864b
Rising <rising1008@gmail.com>
2023-10-15 22:20:28 +0900

    feat: add plugins of vim

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 : --------------------------------------------------
-:  Constraints
+: Constraints
 : --------------------------------------------------
 VERSION="0.1.0"
 OS_TYPE="$(uname -s)"
-TIMESTAMP=$(date  "+%Y%m%d-%H%M%S")
+TIMESTAMP=$(date "+%Y%m%d-%H%M%S")
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)"
 BACKUP_DIR="${HOME}/.backup/${TIMESTAMP}"
 ORIGIN_BACKUP_DIR="${HOME}/.backup/origin"
@@ -18,136 +18,136 @@ readonly BACKUP_DIR
 readonly ORIGIN_BACKUP_DIR
 
 : --------------------------------------------------
-:  Load libs
+: Load libs
 : --------------------------------------------------
 source "${SCRIPT_DIR}/lib/utils.sh"
 
 : --------------------------------------------------
-:  Functions
+: Functions
 : --------------------------------------------------
 banner() {
-  p "\n  dotfiles _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/\n" "info"
-  p "\n" "info"
-  p "  VERSION: ${1}\n" "info" "info"
-  p "  OS_TYPE: ${2}\n" "info" "info"
-  p " _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/\n" "info"
-  p "\n" "info"
+    p "\n  dotfiles _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/\n" "info"
+    p "\n" "info"
+    p "  VERSION: ${1}\n" "info" "info"
+    p "  OS_TYPE: ${2}\n" "info" "info"
+    p " _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/\n" "info"
+    p "\n" "info"
 }
 
-function init () {
-  local origin_backup_dir
-  local backup_dir
+function init() {
+    local origin_backup_dir
+    local backup_dir
 
-  origin_backup_dir=$1
-  backup_dir=$2
+    origin_backup_dir=$1
+    backup_dir=$2
 
-  if [[ ! -d ${origin_backup_dir} ]]; then
-    p "  Create ${origin_backup_dir} directory.\n" "info"
-    mkdir -p "${origin_backup_dir}"
-  fi
+    if [[ ! -d ${origin_backup_dir} ]]; then
+        p "  Create ${origin_backup_dir} directory.\n" "info"
+        mkdir -p "${origin_backup_dir}"
+    fi
 
-  if [[ ! -d ${backup_dir} ]]; then
-    p "  Create ${backup_dir} directory.\n" "info"
-    mkdir -p "${backup_dir}"
-  fi
+    if [[ ! -d ${backup_dir} ]]; then
+        p "  Create ${backup_dir} directory.\n" "info"
+        mkdir -p "${backup_dir}"
+    fi
 }
 
-install_homebrew () {
-  [ ! -z `type -p brew` ] || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+install_homebrew() {
+    [ ! -z $(type -p brew) ] || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
 
-install_packages () {
-  brew bundle --file "${SCRIPT_DIR}/settings/darwin/Brewfile"
-  : install imgcat command
-  [ -f "${SCRIPT_DIR}/bin/imgcat" ] || curl https://raw.githubusercontent.com/gnachman/iTerm2/master/tests/imgcat > "${SCRIPT_DIR}/bin/imgcat"
-  [ -f "${SCRIPT_DIR}/bin/imgcat" ] && chmod 755 "${SCRIPT_DIR}/bin/imgcat"
+install_packages() {
+    brew bundle --file "${SCRIPT_DIR}/settings/darwin/Brewfile"
+    : install imgcat command
+    [ -f "${SCRIPT_DIR}/bin/imgcat" ] || curl https://raw.githubusercontent.com/gnachman/iTerm2/master/tests/imgcat >"${SCRIPT_DIR}/bin/imgcat"
+    [ -f "${SCRIPT_DIR}/bin/imgcat" ] && chmod 755 "${SCRIPT_DIR}/bin/imgcat"
 }
 
-setup_neovim () {
-  if [[ ! -d ~/.config/nvim ]]; then
-    ln -snf "${SCRIPT_DIR}/settings/nvim" "${HOME}/.config/nvim"
-  fi
+setup_neovim() {
+    if [[ ! -d ~/.config/nvim ]]; then
+        ln -snf "${SCRIPT_DIR}/settings/nvim" "${HOME}/.config/nvim"
+    fi
 }
 
-setup_zsh () {
-  if [[ ! -L ${HOME}/.zshrc ]]; then
-    ln -snf "${SCRIPT_DIR}/settings/zsh/.zshrc" "${HOME}"
-  fi
+setup_zsh() {
+    if [[ ! -L ${HOME}/.zshrc ]]; then
+        ln -snf "${SCRIPT_DIR}/settings/zsh/.zshrc" "${HOME}"
+    fi
 
-  if [[ ! -L ${HOME}/.zshenv ]]; then
-    ln -snf "${SCRIPT_DIR}/settings/zsh/.zshenv" "${HOME}"
-  fi
+    if [[ ! -L ${HOME}/.zshenv ]]; then
+        ln -snf "${SCRIPT_DIR}/settings/zsh/.zshenv" "${HOME}"
+    fi
 
-  if [[ ! -d ${HOME}/.zsh.d ]]; then
-    ln -snf "${SCRIPT_DIR}/settings/zsh/.zsh.d" "${HOME}"
-  fi
+    if [[ ! -d ${HOME}/.zsh.d ]]; then
+        ln -snf "${SCRIPT_DIR}/settings/zsh/.zsh.d" "${HOME}"
+    fi
 }
 
-setup_vscode () {
-  local BACKUP_DIR=$1
+setup_vscode() {
+    local BACKUP_DIR=$1
 
-  : backup
-  cp "${HOME}/Library/Application\ Support/Code/User/keybindings.json" "${BACKUP_DIR}/keybindings.json"
-  cp "${HOME}/Library/Application\ Support/Code/User/settings.json" "${BACKUP_DIR}/settings.json"
-  #ln -snf ${TARGET_DIR}/settings/vscode/keybindings.json ${HOME}/Library/Application\ Support/Code/User/keybindings.json
-  #ln -snf ${TARGET_DIR}/settings/vscode/settings.json ${HOME}/Library/Application\ Support/Code/User/settings.json
-  code --install-extension asvetliakov.vscode-neovim
-  code --install-extension EditorConfig.EditorConfig
+    : backup
+    cp "${HOME}/Library/Application\ Support/Code/User/keybindings.json" "${BACKUP_DIR}/keybindings.json"
+    cp "${HOME}/Library/Application\ Support/Code/User/settings.json" "${BACKUP_DIR}/settings.json"
+    #ln -snf ${TARGET_DIR}/settings/vscode/keybindings.json ${HOME}/Library/Application\ Support/Code/User/keybindings.json
+    #ln -snf ${TARGET_DIR}/settings/vscode/settings.json ${HOME}/Library/Application\ Support/Code/User/settings.json
+    code --install-extension asvetliakov.vscode-neovim
+    code --install-extension EditorConfig.EditorConfig
 }
 
-setup_iterms () {
-  : backup
-  if [ -f "${ORIGIN_BACKUP_DIR}/com.googlecode.iterm2.plist" ]; then
-    cp "${HOME}/Library/Preferences/com.googlecode.iterm2.plist" "${BACKUP_DIR}/com.googlecode.iterm2.plist"
-  else
-    cp "${HOME}/Library/Preferences/com.googlecode.iterm2.plist" "${ORIGIN_BACKUP_DIR}/com.googlecode.iterm2.plist"
-  fi
-  : setup
-  cp "${SCRIPT_DIR}/settings/darwin/com.googlecode.iterm2.plist" "${HOME}/Library/Preferences/com.googlecode.iterm2.plist"
+setup_iterms() {
+    : backup
+    if [ -f "${ORIGIN_BACKUP_DIR}/com.googlecode.iterm2.plist" ]; then
+        cp "${HOME}/Library/Preferences/com.googlecode.iterm2.plist" "${BACKUP_DIR}/com.googlecode.iterm2.plist"
+    else
+        cp "${HOME}/Library/Preferences/com.googlecode.iterm2.plist" "${ORIGIN_BACKUP_DIR}/com.googlecode.iterm2.plist"
+    fi
+    : setup
+    cp "${SCRIPT_DIR}/settings/darwin/com.googlecode.iterm2.plist" "${HOME}/Library/Preferences/com.googlecode.iterm2.plist"
 }
 
-setup_git () {
-  ln -snf "${SCRIPT_DIR}/settings/git/.gitconfig" "${HOME}"
+setup_git() {
+    ln -snf "${SCRIPT_DIR}/settings/git/.gitconfig" "${HOME}"
 }
 
-setup_macos () {
-  ${SCRIPT_DIR}/bin/.macos
+setup_macos() {
+    ${SCRIPT_DIR}/bin/.macos
 }
 
 function setup_tmux() {
-  ln -snf "${SCRIPT_DIR}/settings/darwin/.tmux.conf" "${HOME}/.tmux.conf"
+    ln -snf "${SCRIPT_DIR}/settings/darwin/.tmux.conf" "${HOME}/.tmux.conf"
 }
 
 : --------------------------------------------------
-:  Main
+: Main
 : --------------------------------------------------
 banner "${VERSION}" "${OS_TYPE}"
 
 init "${ORIGIN_BACKUP_DIR}" "${BACKUP_DIR}"
 
-p '  Install homebrew...\n' 'info';
+p '  Install homebrew...\n' 'info'
 install_homebrew
 
-p '  Install packages...\n' 'info';
-# install_packages
+p '  Install packages...\n' 'info'
+install_packages
 
-p '\n  Set up zsh...\n';
+p '\n  Set up zsh...\n'
 # setup_zsh
 
-printf -- '  Set up neovim...\n';
+printf -- '  Set up neovim...\n'
 # setup_neovim
 
-printf -- '  Set up iTerms...\n';
+printf -- '  Set up iTerms...\n'
 # 設定ファイルの場所を指定することで対応
 # setup_iterms "${SCRIPT_DIR}" "${BACKUP_DIR}"
 
-printf -- '  Set up tmux...\n';
+printf -- '  Set up tmux...\n'
 setup_tmux
-printf -- '  Set up git...\n';
+printf -- '  Set up git...\n'
 setup_git
 
 # printf -- '  Set up VS Code...\n';
 # setup_vscode "${BACKUP_DIR}"
 
-printf -- '  Set up Mac OS...\n';
+printf -- '  Set up Mac OS...\n'
 # setup_macos
---
+++ settings/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,42 @@
+return {
+	"stevearc/conform.nvim",
+	lazy = true,
+	event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
+	config = function()
+		local conform = require("conform")
+		local util = require("conform.util")
+		util.add_formatter_args(require("conform.formatters.shfmt"), { "-i", "4" })
+
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				svelte = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				graphql = { "prettier" },
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				sh = { "shfmt" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
+}
